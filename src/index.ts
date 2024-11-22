d687e60c29d7f8ee713a2b906674e899b875c05b
Henry Bley-Vroman <olets@olets.dev>
2024-11-22 10:43:25 -1000

    feat(plugin): reintroduce types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// @ts-nocheck
 import { unescapeAll, escapeHtml } from "markdown-it/lib/common/utils.js";
+import type { RenderRule } from "markdown-it/lib/renderer";
 
-const wrapperlessFenceRule = (tokens, idx, options, _env, _slf) => {
+const wrapperlessFenceRule: RenderRule = (tokens, idx, options, _env, _slf) => {
   /**
    * Begin https://github.com/markdown-it/markdown-it/blob/14.1.0/lib/renderer.mjs#L30-L46
    */
